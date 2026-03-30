db7eedf04239477ff985566d8a9b828ae9a822cb
RocioCruzCer <rocicruzcer@gmail.com>
2026-03-30 15:55:17 -0600

    correcion de validaciones

--- app/models/permisos_perfil.rb
+++ app/models/permisos_perfil.rb
@@ -1,5 +1,5 @@
 class PermisosPerfil < ApplicationRecord
-  # Indicamos manualmente las llaves foráneas para que Rails no se pierda
+  # se indica manualmente las llaves foráneas para que Rails no se pierda
   belongs_to :perfil, foreign_key: 'idPerfil'
   belongs_to :modulo, foreign_key: 'idModulo'
 end
